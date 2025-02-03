ccf5e51dc3751e3bbf9f7b3593bd619369a29943
RUSHEEK1 <131938270+RUSHEEK1@users.noreply.github.com>
2025-02-03 10:17:14 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const input = document.getElementById("input")
+
+function reverseString(str) {
+    return str.split("").reverse().join("")
+}
+function check() {
+    const value = input.value
+    const reverse = reverseString(value)
+
+    if (value === reverse) {
+        alert("P A L I N D R O M E spoted")
+    } else{
+        alert("Not This Time!")
+    }
+}
